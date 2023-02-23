0b368ad878c2801c1d78506eb240079d37a4f1ca
Tim Cai <tim.j.cai@gmail.com>
2023-02-23 16:32:13 +1100

    styled the banner and navbar

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,6 +1,7 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
+  has_many_attached :photos
 
   validates :movie_id, uniqueness: { scope: :list_id }
   validates :comment, presence: true, length: { minimum: 6 }
